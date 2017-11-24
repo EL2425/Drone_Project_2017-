c4870a540969185f6d5ff7eb47e9e3f09c5a1055
el2425-PC <el2425@unkown.com>
2017-11-24 16:05:18 +0100

    Added error computation between reference and position output and display of max error and MSE on x,y,z

--- bagfiles/plot_data.py
+++ bagfiles/plot_data.py
@@ -22,6 +22,7 @@ def trajectoryPlot( bagfile, crazyflie):
 		msg_tg.append(m)
 		msg_tg_t.append(float(t.secs) + float(t.nsecs) / 1e9)
 
+
 	#t0=msg_t[0]
 	#msg_t = [t-t0 for t in msg_t] #Removing the initial time to start at 0
 	#t_tg0=msg_tg_t[0]
@@ -29,6 +30,9 @@ def trajectoryPlot( bagfile, crazyflie):
 	bag.close()
 	#print(msg_t)
 	#print(type(msg_t[0]))
+
+	
+
 
 	x = [tw.linear.x for tw in msg]
 	y = [tw.linear.y for tw in msg]
@@ -38,10 +42,36 @@ def trajectoryPlot( bagfile, crazyflie):
 	y_tg = [tw.linear.y for tw in msg_tg]
 	z_tg = [tw.linear.z for tw in msg_tg]
 
+	print(len(msg_t))
+	print(len(msg_tg_t))
+
+	errX=[]
+	errY=[]
+	errZ=[]
+
+	commonTime=[min(range(len(msg_t)),key=lambda i: abs(msg_t[i]-time)) for time in msg_tg_t]
+
+	for i in range(len(msg_tg_t)):
+	#errX.append(abs(x[i]-x_tg[i]) np.where (msg_t==msg_tg_t))
+		errX.append(abs(x[commonTime[i]]-x_tg[i]))
+		errY.append(abs(y[commonTime[i]]-y_tg[i]))
+		errZ.append(abs(z[commonTime[i]]-z_tg[i]))
+
+
+	maxErrX = np.max(errX)
+	maxErrY = np.max(errY)
+	maxErrZ = np.max(errZ)
+
+	MSEx = np.mean([err*err for err in errX])
+	MSEy = np.mean([err*err for err in errY])
+	MSEz = np.mean([err*err for err in errZ])
+
+	
 	fig=plt.figure()
 	ax=fig.add_subplot(111, projection='3d')
 	ax.plot(x, y, z)
 	ax.plot(x_tg, y_tg, z_tg)
+
 
 	fig2=plt.figure()
 	#ax2=fig2.add_subplot(311)
@@ -59,6 +89,25 @@ def trajectoryPlot( bagfile, crazyflie):
 	plt.plot(msg_t, z)
 	plt.plot(msg_tg_t, z_tg)
 	plt.ylabel('z')
+	plt.xlabel('time (s)')
+
+
+	fig3=plt.figure()
+	#ax2=fig2.add_subplot(311)
+	plt.subplot(311)
+	plt.plot(msg_tg_t,errX)
+	plt.ylabel('Absolute error on x')
+	plt.title('Max err = '+ str(maxErrX)+'    MSE = '+str(MSEx))
+	#ax3=fig2.add_subplot(312)
+	plt.subplot(312)
+	plt.plot(errY)
+	plt.ylabel('Absolute error on y')
+	plt.title('Max err = '+ str(maxErrY)+'    MSE = '+str(MSEy))
+	#ax4=fig2.add_subplot(313)
+	plt.subplot(313)
+	plt.plot(errZ)
+	plt.ylabel('Absolute error on z')
+	plt.title('Max err = '+ str(maxErrZ)+'    MSE = '+str(MSEz))
 	plt.xlabel('time (s)')
 
 
